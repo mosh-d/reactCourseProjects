c74fac5f63238d459ff90deef63aedf853b819d4
mosh-d <shopsy8@gmail.com>
2025-02-21 19:00:39 +0100

    Adding forwarded props to the sections component

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 
-import TabButton from './TabButton';
-import { EXAMPLES } from '../data';
+import TabButton from './TabButton.jsx';
+import Section from './Section.jsx';
+import { EXAMPLES } from '../data.js';
 
 export default function Examples() {
   const [ selectedTopic, setSelectedTopic ] = useState();
@@ -26,13 +27,12 @@ export default function Examples() {
   }
 
   return (
-  <section id="examples">
-    <h2>Examples</h2>
+  <Section title="Examples" id="examples">
     <menu>
-      <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-      <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JXS</TabButton>
-      <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-      <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+      <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+      <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JXS</TabButton>
+      <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
+      <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
     </menu>
 
     {tabContent}
@@ -50,6 +50,6 @@ export default function Examples() {
         </pre>
       </div>
       )} */}
-  </section>
+  </Section>
   )
 }
--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -3,10 +3,10 @@
 } */
 
 //Using destructuring
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+      <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
     </li>
   );
 }
