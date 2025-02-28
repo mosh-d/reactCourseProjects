ad151c7b9a253da98fbc4762e93e544a16569514
mosh-d <shopsy8@gmail.com>
2025-02-28 12:09:11 +0100

    Adding multiple JSX slots

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import TabButton from './TabButton.jsx';
 import Section from './Section.jsx';
+import Tabs from './Tabs.jsx';
 import { EXAMPLES } from '../data.js';
 
 export default function Examples() {
@@ -28,28 +29,18 @@ export default function Examples() {
 
   return (
   <Section title="Examples" id="examples">
-    <menu>
-      <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
-      <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JXS</TabButton>
-      <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
-      <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
-    </menu>
-
-    {tabContent}
-    
-    {/* Alternative to the if statement above */}
-
-  {/*           {!selectedTopic ? (
-      <p>Please select a topic.</p>
-    ) : (
-    <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-      )} */}
+    <Tabs 
+      buttons={
+        <>
+          <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+          <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JXS</TabButton>
+          <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
+          <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
+        </>
+      }
+    >
+      {tabContent}
+    </Tabs>
   </Section>
   )
 }
--- 01-starting-project/src/components/Section.jsx
+++ 01-starting-project/src/components/Section.jsx
@@ -4,5 +4,5 @@ export default function Section({ title, children, ...props }) {
       <h2>{title}</h2>
       {children}
     </section>
-  ) 
+  ) ;
 }
