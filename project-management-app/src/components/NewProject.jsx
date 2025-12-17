efb8dab58b00b8baa40851179cdeb3bb85d09208
mosh-d <shopsy8@gmail.com>
2025-12-17 13:37:11 +0100

    Updating project management app

--- project-management-app/src/components/NewProject.jsx
+++ project-management-app/src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import Input from "./Input.jsx";
 import Modal from "./Modal.jsx";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
 
   const title = useRef();
@@ -35,18 +35,18 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} buttonCaption={"Okay"}>
-        <h2 className="text-xl font-bold yext-stone-500 my-4">Invalid Input</h2>
-        <p className="text-stone-400 mb-4">
+        <h2 className="text-xl font-bold yext-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
           Ooops... lookslike you forgot to enter a value.
         </p>
-        <p className="text-stone-400 mb-4">
+        <p className="text-stone-600 mb-4">
           Please make sure you provide a valid value for every input field.
         </p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
               Cancel
             </button>
           </li>
