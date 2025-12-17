844f93b5ca9bbb63979c662c04c8f92100f49450
mosh-d <shopsy8@gmail.com>
2025-12-17 13:30:27 +0100

    Updating project management app

--- project-management-app/src/components/NewProject.jsx
+++ project-management-app/src/components/NewProject.jsx
@@ -35,9 +35,13 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} buttonCaption={"Okay"}>
-        <h2>Invalid Input</h2>
-        <p>Ooops... lookslike you forgot to enter a value.</p>
-        <p>Please make sure you provide a valid value for every input field.</p>
+        <h2 className="text-xl font-bold yext-stone-500 my-4">Invalid Input</h2>
+        <p className="text-stone-400 mb-4">
+          Ooops... lookslike you forgot to enter a value.
+        </p>
+        <p className="text-stone-400 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
