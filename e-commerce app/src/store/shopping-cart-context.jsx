59fb5ffab0f865a46c45b8267368a95deebc8c79
mosh-d <shopsy8@gmail.com>
2025-12-18 13:21:31 +0100

    Updating e-commerce app

--- e-commerce app/src/store/shopping-cart-context.jsx
+++ e-commerce app/src/store/shopping-cart-context.jsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+
+import { DUMMY_PRODUCTS } from '../dummy-products.js';
+
 import { createContext } from 'react';
 
 export const CartContext = createContext({
@@ -5,3 +9,75 @@ export const CartContext = createContext({
   addItemToCart: () => {}, //for auto-completion
   updateItemQuantity: () => {} //for auto-completion
 });
+
+export default function CartContextProvider({children}) {
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
+
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity
+  };
+
+  return <CartContext value={ctxValue}>
+    {children}
+  </CartContext>
+}
